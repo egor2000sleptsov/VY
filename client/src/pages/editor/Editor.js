e8c04b4b8945be7f035d72ca525e6211b4047aee
Egor <egor2000sleptsov@gmail.com>
2021-04-30 12:59:40 +1000

    30.04
    add shape LIne, maded flux for Queue and Shape

--- client/src/pages/editor/Editor.js
+++ client/src/pages/editor/Editor.js
@@ -1,71 +1,122 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import s from './Editor.module.css'
 // import ButtonShape from "./ButtonShape/ButtonShape";
 import {Layer, Line, Rect, Stage} from "react-konva";
 
 
 function Editor(props) {
+    const formatQueue = (queue) => {
+        const a = queue.map(el => {
+            switch (el.type) {
+                case "Line":
+                    return (<Line x={el.x} y={el.y} key={el.id} ref={{current: null}} stroke="black" points={[0, 0, el.width, el.height]}/>)
+                case "Rect":
+                    return (<Rect x={el.x} y={el.y} key={el.id} ref={{current: null}} stroke="black" width={el.width} height={el.height} />)
+                default:
+                    return false
+            }
+        })
+        return a
+    }
     const canvas = React.createRef()
     const layer = React.createRef()
     const shapeRef = useRef(null)
     const [drawing, setDrawing] = useState(props.drawing)
-    const [queue, setQueue] = useState([])
+    const queue = formatQueue(props.queue)
     const [shape, setShape] = useState(null)
 
-    const mouseDownHandler = (e) => {
+    const mouseDownHandler = e => {
         setDrawing(true)
         props.setDrawing(!drawing)
-
-
-        setShape(<Rect
-            x={canvas.current.getPointerPosition().x}
-            y={canvas.current.getPointerPosition().y}
-            width={0}
-            height={0}
-            stroke="black"
-            ref={shapeRef}
-        />)
+        switch (props.currentShape) {
+            case "Line":
+                setShape(<Line
+                    x={canvas.current.getPointerPosition().x}
+                    y={canvas.current.getPointerPosition().y}
+                    points={[0, 0, 0, 0]}
+                    closed
+                    stroke="black"
+                    ref={shapeRef}
+                />)
+                return false
+            case "Rect":
+                setShape(<Rect
+                    x={canvas.current.getPointerPosition().x}
+                    y={canvas.current.getPointerPosition().y}
+                    width={0}
+                    height={0}
+                    stroke="black"
+                    ref={shapeRef}
+                />)
+                return false
+            default:
+                return false
+        }
 
     }
-    const mouseMoveHandler = (e) => {
+
+    const mouseMoveHandler = e => {
         if (!drawing) return false
         const newWidth = canvas.current.getPointerPosition().x - shape.props.x
         const newHeight = canvas.current.getPointerPosition().y - shape.props.y
-        setShape(<Rect
-            x={shape.props.x}
-            y={shape.props.y}
-            width={newWidth}
-            height={newHeight}
-            stroke="black"
-            ref={shapeRef}
-        />)
-        shapeRef.current.to({
-            width: canvas.current.getPointerPosition().x - shape.props.x,
-            height: canvas.current.getPointerPosition().y - shape.props.y
-        })
-        // const newWidth = canvas.current.getPointerPosition().x - shape.props.x
-        // const newHeight = canvas.current.getPointerPosition().y - shape.props.y
-        // setShape(<Rect
-        //     x={shape.props.x}
-        //     y={shape.props.y}
-        //     width={newWidth}
-        //     height={newHeight}
-        //     stroke="black"
-        // />)
+        switch (props.currentShape) {
+            case "Line":
+                setShape(<Line
+                    x={shape.props.x}
+                    y={shape.props.y}
+                    closed
+                    points={[0, 0, newWidth, newHeight]}
+                    stroke="black"
+                    ref={shapeRef}
+                />)
+                return false
+            case "Rect":
+                setShape(<Rect
+                    x={shape.props.x}
+                    y={shape.props.y}
+                    width={newWidth}
+                    height={newHeight}
+                    stroke="black"
+                    ref={shapeRef}
+                />)
+                return false
+            default:
+                return false
+        }
+
     }
 
-    const mouseUpHandler = (e) => {
+    const mouseUpHandler = e => {
         setDrawing(false)
         props.setDrawing(!drawing)
-        setQueue([...queue, shape])
+        if (shape) {
+            switch (shape.type) {
+                case "Rect":
+                    props.setQueue({
+                        id: queue.length,
+                        type: shape.type,
+                        x: shape.props.x,
+                        y: shape.props.y,
+                        width: shape.props.width,
+                        height: shape.props.height
+                    })
+                    return false
+                case "Line":
+                    props.setQueue({
+                        id: queue.length,
+                        type: shape.type,
+                        x: shape.props.x,
+                        y: shape.props.y,
+                        width: shape.props.points[2],
+                        height: shape.props.points[3]
+                    })
+            }
+
+        }
         setShape(null)
     }
 
-    const debug = () => {
-        debugger
-    }
-
-    const mouseOut = () => {
+    const mouseOutHandler = e => {
         if (drawing) {
             setDrawing(false)
             props.setDrawing(!drawing)
@@ -73,10 +124,19 @@ function Editor(props) {
         }
     };
 
+    const debug = e => {
+        debugger
+    }
+
+    const buttons = props.shapes.map(el => (
+        <button onClick={() => props.setCurrentShape(el)} key={props.shapes.indexOf(el)}>{el}</button>))
+
     return (
         <div>
             <button onClick={debug}>debugger</button>
-            <div onMouseOut={mouseOut}>
+            {buttons}
+
+            <div onMouseOut={mouseOutHandler}>
                 <Stage
                     className={s.canvas}
                     ref={canvas}
